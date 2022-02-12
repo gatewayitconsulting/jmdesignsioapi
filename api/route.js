79d5a847131899c27fd327ef705a4a4a7c102747
Chip Thien <themacrochip@gmail.com>
2022-02-11 23:40:02 -0600

    Sanitize rsvp posts

--- api/route.js
+++ api/route.js
@@ -16,7 +16,18 @@ router.get('/rsvps', async (req, res) => {
 router.post('/rsvps', async (req, res) => {
     console.log(`Received rsvps post with body:`, req.body);
     try {
-        const insertResult = await Rsvps.insertRSVP(req.body);
+        const body = req.body;
+        const insertResult = await Rsvps.insertRSVP({
+            firstName: body.firstName,
+            lastName: body.lastName,
+            email: body.email,
+            phone: body.phone,
+            diet: body.diet,
+            songRequest: body.songRequest,
+            notes: body.notes,
+            guestFirstName: body.guestFirstName,
+            guestLastName: body.guestLastName,
+        });
         console.log(`Successfully inserted ${insertResult.rowCount} row(s)`);
         res.sendStatus(200);
     } catch (e) {
